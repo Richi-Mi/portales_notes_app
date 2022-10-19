9629cbd5b946af89cab6bf3c08e9be1959f11b20
José Ricardo Mendoza <joseanakin@outlook.es>
2022-10-19 18:00:12 -0500

    Navigator Terminado

--- js/interface.js
+++ js/interface.js
@@ -1,10 +1,14 @@
 const aside = document.querySelector('aside');
 const btn_close = document.querySelector('.btn_close');
 const container = document.querySelector('.container');
+const in_name      = document.querySelector('#name');
 
 let abierto = false;
+let nombre = localStorage.getItem('nombre');
 
-btn_close.addEventListener('click', (e) => {
+nombre != null ? in_name.value = nombre : in_name.value = '';
+
+btn_close.addEventListener('click', () => {
     abierto = !abierto;
 
     if( abierto ) {
@@ -15,4 +19,10 @@ btn_close.addEventListener('click', (e) => {
         aside.classList.remove('cerrar');
         container.classList.remove('not_aside');
     }
+});
+
+in_name.addEventListener('focusout', () => {
+    nombre = in_name.value;
+
+    localStorage.setItem('nombre', nombre );
 })
