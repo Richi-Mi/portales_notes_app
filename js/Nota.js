de37d956134b974a344df4943602fff900aa2c8d
José Ricardo Mendoza <joseanakin@outlook.es>
2022-10-20 06:10:26 -0500

    Addnote.js added

--- js/Nota.js
+++ js/Nota.js
@@ -2,6 +2,25 @@
 class Nota {
     constructor() {
         this.titulo;
+        this.elements = [];
+    }
+    addElement( type, content ) {
+        const el = {
+            type,
+            content
+        }
+        this.elements.push( el );
+    }
+    removeElement( ind ) {
+        let newElements;
+        for( let element in this.elements ) {
+            if( element !== ind ) {
+                newElements.push( this.elements[element] );
+            }
+        }
+    }
+    getElements() {
+        return this.elements;
     }
 }
 export default Nota;
---
+++ js/addNote.js
@@ -0,0 +1,15 @@
+import Nota from "./Nota.js";
+
+const add_parr = document.querySelector('.add_title');
+const add_title = document.querySelector('.add_parr');
+const new_note  = document.querySelector('.new_note');
+
+const nota = new Nota();
+
+add_parr.addEventListener('click', () => {
+    
+});
+
+add_title.addEventListener('click', () => {
+
+});
